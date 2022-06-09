346a5146b61da97fb54eccc74a120657f4d6259f
Joseph Stephens <jstephe123@gmail.com>
2022-06-09 10:06:45 -0400

    1st update

---
+++ src/main/java/com/techelevator/VendingItems.java
@@ -0,0 +1,51 @@
+package com.techelevator;
+
+import java.math.BigDecimal;
+
+public class VendingItems {
+
+    private String buttonNumber;
+    private String itemName;
+    private BigDecimal price;
+    private String itemType;
+
+    public String getButtonNumberNumber(String buttonNumber){
+        return buttonNumber;
+    }
+
+    public void setButtonNumber(String buttonNumber){
+        this.buttonNumber = buttonNumber;
+    }
+
+    public String getItemName(String itemName){
+        return itemName;
+    }
+
+    public void setItemName(String itemName){
+        this.itemName = itemName;
+    }
+
+    public BigDecimal getPrice(BigDecimal price){
+        return price;
+    }
+
+    public void setPrice(BigDecimal price){
+        this.price = price;
+    }
+
+    public String getItemType(String itemType){
+        return itemType;
+    }
+
+    public void setItemType(String itemType){
+        this.itemType = itemType;
+    }
+
+
+    
+
+
+
+
+
+}
