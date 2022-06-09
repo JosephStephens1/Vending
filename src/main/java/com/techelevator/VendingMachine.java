d45b758a37681d6a9e9a61636f98a10db4bcdbb7
Tashir Stover <tashiana.stover@gmail.com>
2022-06-09 12:13:25 -0400

    Vending machine class adds

--- src/main/java/com/techelevator/VendingMachine.java
+++ src/main/java/com/techelevator/VendingMachine.java
@@ -1,4 +1,27 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class VendingMachine {
+
+    // Instance Variables
+String input = "C:\\Users\\Student\\workspace\\nlr-8-module-1-capstone-orange-team-9\\vendingmachine.csv";
+
+    // Constructor
+    public VendingMachine() throws FileNotFoundException {
+
+     try(  Scanner inventoryInput = new Scanner(input)) {
+         while (inventoryInput.hasNextLine()) {
+             String currentLine = inventoryInput.nextLine();
+             System.out.println(currentLine);
+         }
+     }
+
+    }
+
+
+
+
 }
